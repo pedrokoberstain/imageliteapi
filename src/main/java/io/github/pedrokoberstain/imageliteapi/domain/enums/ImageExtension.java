80e0c2b6e5d6034bf17fcb55aef98b0639adfba1
pedro <pedrokoberstain2@gmail.com>
2024-08-24 08:18:07 -0400

    feat: Salvando a imagem atráves da API, com a lógica para adquirir e demonstrar o seu tipo.

--- src/main/java/io/github/pedrokoberstain/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/io/github/pedrokoberstain/imageliteapi/domain/enums/ImageExtension.java
@@ -1,8 +1,25 @@
 package io.github.pedrokoberstain.imageliteapi.domain.enums;
 
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+
 public enum ImageExtension {
-    PNG,
-    GIF,
-    JPG,
-    JPEG
+
+    PNG(MediaType.IMAGE_PNG),
+    GIF(MediaType.IMAGE_GIF),
+    JPEG(MediaType.IMAGE_JPEG);
+
+    private final MediaType mediaType;
+
+    ImageExtension(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType) {
+        return Arrays.stream(values())
+                .filter(ie ->ie.mediaType.equals(mediaType))
+                .findFirst()
+                .orElse(null);
+    }
 }
--- src/main/java/io/github/pedrokoberstain/imageliteapi/domain/service/ImageService.java
+++ src/main/java/io/github/pedrokoberstain/imageliteapi/domain/service/ImageService.java
@@ -1,7 +1,6 @@
 package io.github.pedrokoberstain.imageliteapi.domain.service;
 
 import io.github.pedrokoberstain.imageliteapi.domain.entity.Image;
-import jakarta.transaction.Transactional;
 
 public interface ImageService {
     Image save(Image image);
