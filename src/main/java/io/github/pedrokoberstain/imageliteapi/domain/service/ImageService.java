681ae737998d955f90146008cf8586217ec12f3f
pedro <pedrokoberstain2@gmail.com>
2024-09-03 06:15:11 -0400

    feat: Criando o serviço de pesquisa

--- src/main/java/io/github/pedrokoberstain/imageliteapi/domain/service/ImageService.java
+++ src/main/java/io/github/pedrokoberstain/imageliteapi/domain/service/ImageService.java
@@ -1,11 +1,15 @@
 package io.github.pedrokoberstain.imageliteapi.domain.service;
 
 import io.github.pedrokoberstain.imageliteapi.domain.entity.Image;
+import io.github.pedrokoberstain.imageliteapi.domain.enums.ImageExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface  ImageService {
     Image save(Image image);
 
     Optional<Image> getById(String id);
+
+    List<Image> search(ImageExtension extension, String query);
 }
