1f8acd9ef722366e756b45bbe8e34b6c8eb7f1cc
pedro <pedrokoberstain2@gmail.com>
2024-09-03 06:15:16 -0400

    feat: Criando o serviço de pesquisa

--- src/main/java/io/github/pedrokoberstain/imageliteapi/application/images/ImageServiceImpl.java
+++ src/main/java/io/github/pedrokoberstain/imageliteapi/application/images/ImageServiceImpl.java
@@ -1,12 +1,14 @@
 package io.github.pedrokoberstain.imageliteapi.application.images;
 
 import io.github.pedrokoberstain.imageliteapi.domain.entity.Image;
+import io.github.pedrokoberstain.imageliteapi.domain.enums.ImageExtension;
 import io.github.pedrokoberstain.imageliteapi.domain.service.ImageService;
 import io.github.pedrokoberstain.imageliteapi.infra.repository.ImageRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,5 +26,10 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public Optional<Image> getById(String id) {
         return repository.findById(id);
+    }
+
+    @Override
+    public List<Image> search(ImageExtension extension, String query) {
+        return repository.findByExtensionAndNameOrTagsLike(extension, query);
     }
 }
